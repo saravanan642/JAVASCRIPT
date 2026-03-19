b5012729ed6eabea8cf951c38556a288fc9624fc
saravanan642 <saravanansenthil605@gmail.com>
2026-03-19 15:21:27 +0530

    add data types and notes folder

---
+++ DataTypes/number.js
@@ -0,0 +1,18 @@
+// let a= 40;
+// let b= 50;
+// let c= 60;
+// console.log("sum", a+b+c)
+
+// var d = 6.9;
+// let t = 7.9;
+// console.log( "sum",d-t)
+
+// const srt = "100";
+// const num = Number(srt)
+// console.log (srt)
+
+var o=100;
+var g =String(o);
+console.log(o)
+console.log(typeof g);
+
