26d2914ed99f0309bdcaa54be9abe8203d18f3f9
saravanan642 <saravanansenthil605@gmail.com>
2026-03-30 11:31:41 +0530

    stat tha control statement

---
+++ Conditional_Statements/ifstatement.js
@@ -0,0 +1,38 @@
+ let age = 35;
+ if (age>20){
+ console.log("voting");
+ }
+
+
+ var number = 60;
+ if(number%2 === 0){
+    console.log(" that is even number ");
+ }
+
+ var  num = 100;
+  if ( num /2 ==50){
+    console.log("divied tha number ")
+  }
+
+  let num1= "90";
+  if (num1 == 90){
+    console.log (" welcome to nandha ")
+
+  }
+
+
+  let id = 20;
+  let id1 = 30;
+   if (id < 30 && id1>20){
+    console.log(" tha values is true")
+   }
+
+   
+
+   let sara = 20;
+
+if(sara >= 18){
+    if(sara <= 50){
+        console.log("Eligible for all");
+    }
+}
