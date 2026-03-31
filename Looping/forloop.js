c57dc5ea9017bcb21f32f6ce9383921cda847e05
saravanan642 <saravanansenthil605@gmail.com>
2026-03-31 12:22:15 +0530

    start the looping

---
+++ Looping/forloop.js
@@ -0,0 +1,51 @@
+// for(i=0; i<10; i++){
+//     console.log(" tha i valuse is",(i));
+// }
+
+// for ( i=0; i>=-10; i--){
+//     console.log(i);
+// }
+
+// let a= 10 , b=5;
+// for(a; b<=a; a--){
+//     console.log(a);
+// }
+
+// let  a =0;
+  
+// for (  i=0; i<=15; i++){
+//     sum = a + i;
+   
+// }
+//  console.log(sum);
+
+// let  a =0;
+
+//  for (  i=0; i<=15; i++){
+//     sum = a + i;
+//     console.log(sum);
+
+// }
+
+
+// let a = 10 ;
+// let b = 20 ;
+// let c= 30;
+
+// var store = a+b+c;
+
+
+// for (i =0 ; i<40; i++){
+//      store = i
+//      console.log(store);
+// }
+
+
+let a = 2;
+let b=3;
+let fact= a+b; 
+
+for (i=1; i<=10; i++){
+fact *=i
+console.log(fact);
+}
